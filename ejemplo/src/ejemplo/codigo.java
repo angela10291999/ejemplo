e877ff8cb2597d092e77eabb513708450cfb9100
Angela <Angela@192.168.1.39>
2017-12-13 22:05:37 -0500

    ejemplo...

---
+++ ejemplo/src/ejemplo/codigo.java
@@ -0,0 +1,15 @@
+package ejemplo;
+
+public class codigo {
+
+	public static void main(String[] args) {
+		{
+			for(int i = 0; i <= 10; i++)
+			{
+				System.out.print("10 * "+ i + " = " + 10 * i + "\n");
+			}
+		}
+	    
+	}
+}
+
